d31c21632c2083b7cda653e990e830bbe0f10778
NotLe0n <notle0n75@gmail.com>
2026-08-17 18:00:56 +0200

    Add CSP headers

--- tmldb-front/svelte.config.js
+++ tmldb-front/svelte.config.js
@@ -3,8 +3,21 @@ import adapter from '@sveltejs/adapter-node';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		csp: {
+			directives: {
+				'default-src': ['self'],
+				'script-src': ['self', 'https://static.cloudflareinsights.com'],
+				'connect-src': ['self', 'https://cloudflareinsights.com'],
+				'worker-src': ['self', 'blob:'],
+				'img-src': ['*', 'data:'],
+				'base-uri': ['self'],
+				'form-action': ['self'],
+				'frame-ancestors': ['none'],
+				'object-src': ['none']
+			}
+		},
+	},
 };
 
 export default config;
